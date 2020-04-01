375caca266b7c75c492116d660c4a31cd339b69b
Alex Watanabe <aowatanabe@gmail.com>
2020-04-01 10:30:13 -0300

    separando rotas em arquivos

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,32 +1,8 @@
-const { check, validationResult } = require('express-validator');
-
-const LivroDao = require('../infra/livro-dao');
-const db = require('../../config/database');
-
-const LivroControlador = require('../controladores/livro-controlador');
-const livroControlador = new LivroControlador();
-
-const BaseControlador = require('../controladores/base-controlador');
-const baseControlador = new BaseControlador();
+const livroRotas = require('./livro-rotas');
+const baseRotas = require('./base-rotas');
 
 
 module.exports = (app) => {
-    const rotasBase = BaseControlador.rotas();
-    const rotasLivro = LivroControlador.rotas();
-
-    app.get(rotasBase.home, baseControlador.home());
-    
-    app.get(rotasLivro.lista, livroControlador.lista());
-    app.get(rotasLivro.cadastro, livroControlador.formularioCadastro());
-    app.get(rotasLivro.edicao, livroControlador.formularioEdicao());
-
-    app.post(rotasLivro.lista, [
-        check('titulo').isLength({ min: 5 }).withMessage('O título precisa ter no mínimo 5 caracteres!'),
-        check('preco').isCurrency().withMessage('O preço precisa ter um valor monetário válido!')
-    ], 
-    livroControlador.cadastra());
-
-    app.put(rotasLivro.lista, livroControlador.edita());
-    app.delete(rotasLivro.delecao, livroControlador.remove());
-    
+    baseRotas(app);
+    livroRotas(app);    
 };
